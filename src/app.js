c5a1acced031c6f35af95fc2365293171bdd451a
klinsmann <lopesklinsmann@outlook.com>
2019-05-16 22:21:05 -0300

    retornado erro 404

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const port = 3000;
 app.set("port", port);
 const server = http.createServer(app);
 
+app.use((request, response, next) => {
+    response.status(400).send();
+});
+
 server.listen(port, host, ()=> {
     console.log("teste");
 });
