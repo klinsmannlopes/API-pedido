324d31d94ca101b834d362682ec907a475830bd5
klinsmann <lopesklinsmann@outlook.com>
2019-05-16 22:16:57 -0300

    baixando express

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const host = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Olá mundo");
-});
+app.set("port", port);
+const server = http.createServer(app);
 
 server.listen(port, host, ()=> {
     console.log("teste");
